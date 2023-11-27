99282d0eccbc93dc11322e090ec46ab931e88d85
yurialmeida24 <yurimendes16@gmail.com>
2023-11-26 21:42:05 -0300

    theguris

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import 'dotenv/config'
 import passport from 'passport';
 import session from 'express-session';
 import { OAuth2Strategy as GoogleStrategy } from 'passport-google-oauth';
+import fs from 'fs';
 
 import { searchFlights } from './integrations/amadeus.js';
 import { makeItinerary } from './integrations/openai.js';
@@ -15,6 +16,7 @@ const port = 3000;
 app.use(cors());
 app.set('view engine', 'ejs');
 app.set('views', './views');
+app.use(express.static('public'));
 
 app.use(session({
   resave: false,
@@ -39,15 +41,79 @@ const isLoggedIn = (req, res, next) => {
 /* Front Routes */
 
 app.get('/search', isLoggedIn, (req, res) => {
-  res.render('index');
+  res.render('index', { flights: [] });
 });
 
 /* Travel routes */
 
+app.get('/results', function(req, res) {
+  // Read the contents of the payload.txt file
+  fs.readFile('payload.txt', 'utf8', function(err, data) {
+    if (err) {
+      console.error('Error reading the payload.txt file:', err);
+      return res.status(500).send('Error loading flight data');
+    }
+
+    try {
+      // Parse the JSON data
+      const payloadData = JSON.parse(data);
+
+      // Process the data to fit the format required by your EJS template
+      const flights = payloadData.data.map((offer) => {
+        // Extract the necessary details from each flight offer
+        // This is just an example, adjust the properties according to your payload structure
+        const segmentsLength = offer.itineraries[0].segments.length
+        const stops = offer.itineraries[0].segments.filter((s, idx, arr)=> arr[0].departure.iataCode != arr[idx].departure.iataCode).map(s => s.departure.iataCode) 
+        console.log(offer.itineraries[0].segments.map(s => s.departure.iataCode) )
+        return {
+          id: offer.id,
+          airline: offer.itineraries[0].segments[0].carrierCode,
+          departureTime: offer.itineraries[0].segments[0].departure.at,
+          arrivalTime: offer.itineraries[0].segments[segmentsLength-1].arrival.at,
+          duration: offer.itineraries[0].duration,
+          departureAirport: offer.itineraries[0].segments[0].departure.iataCode,
+          arrivalAirport: offer.itineraries[0].segments[segmentsLength-1].arrival.iataCode,
+          direct: offer.itineraries[0].segments.length === 1,
+          price: offer.price && offer.price.total, // Assuming there is a price object
+          airlineCode:  offer.itineraries[0].segments[0].carrierCode,
+          stops
+        };
+      });
+
+      // Render the EJS template with the flights data
+      res.render('index', { flights: flights });
+
+    } catch (error) {
+      console.error('Error parsing JSON from payload.txt:', error);
+      return res.status(500).send('Error parsing flight data');
+    }
+  });
+});
+
 app.get('/flights', isLoggedIn, async (req, res) => {
   console.log("GET /flights");
   const flightsData = await searchFlights(req.query);
-  res.status(200).send(flightsData);
+  const flights = flightsData.data.map((offer) => {
+    // Extract the necessary details from each flight offer
+    // This is just an example, adjust the properties according to your payload structure
+    const segmentsLength = offer.itineraries[0].segments.length
+    const stops = offer.itineraries[0].segments.filter((s, idx, arr)=> arr[0].departure.iataCode != arr[idx].departure.iataCode).map(s => s.departure.iataCode) 
+    console.log(offer.itineraries[0].segments.map(s => s.departure.iataCode) )
+    return {
+      id: offer.id,
+      airline: offer.itineraries[0].segments[0].carrierCode,
+      departureTime: offer.itineraries[0].segments[0].departure.at,
+      arrivalTime: offer.itineraries[0].segments[segmentsLength-1].arrival.at,
+      duration: offer.itineraries[0].duration,
+      departureAirport: offer.itineraries[0].segments[0].departure.iataCode,
+      arrivalAirport: offer.itineraries[0].segments[segmentsLength-1].arrival.iataCode,
+      direct: offer.itineraries[0].segments.length === 1,
+      price: offer.price && offer.price.total, // Assuming there is a price object
+      airlineCode:  offer.itineraries[0].segments[0].carrierCode,
+      stops
+    };
+  });
+  res.render('index', { flights: flights });
 });
 
 app.get('/itinerary', isLoggedIn, async (req, res) => {
